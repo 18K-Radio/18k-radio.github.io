385ca9398b3434df0c30777f3cb3ffcb5dd27482
watchakorn-18k <wk18k@proton.me>
2024-11-24 14:32:02 +0700

    fix : github action file

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,7 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  basePath: "/",
   distDir: "build",
   output: "export",
   reactStrictMode: true,
